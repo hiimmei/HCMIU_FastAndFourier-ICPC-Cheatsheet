d116114ae4f0cb9cdd255fd15ca0bc3f00a57c68
Stardayy <109837575+Stardayy@users.noreply.github.com>
2025-10-02 16:11:56 +0700

    Update SegmentTree.h

--- content/data-structures/SegmentTree.h
+++ content/data-structures/SegmentTree.h
@@ -1,31 +1,38 @@
-/**
- * Author: Lucian Bicsi
- * Date: 2017-10-31
- * License: CC0
- * Source: folklore
- * Description: Zero-indexed max-tree. Bounds are inclusive to the left and exclusive to the right.
- * Can be changed by modifying T, f and unit.
- * Time: O(\log N)
- * Status: stress-tested
- */
-#pragma once
-
-struct Tree {
-	typedef int T;
-	static constexpr T unit = INT_MIN;
-	T f(T a, T b) { return max(a, b); } // (any associative fn)
-	vector<T> s; int n;
-	Tree(int n = 0, T def = unit) : s(2*n, def), n(n) {}
-	void update(int pos, T val) {
-		for (s[pos += n] = val; pos /= 2;)
-			s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
-	}
-	T query(int b, int e) { // query [b, e)
-		T ra = unit, rb = unit;
-		for (b += n, e += n; b < e; b /= 2, e /= 2) {
-			if (b % 2) ra = f(ra, s[b++]);
-			if (e % 2) rb = f(s[--e], rb);
-		}
-		return f(ra, rb);
-	}
-};
+//t[v] la tong cac phan tu tu chi so l toi r
+void build(int v, int l, int r){
+    if(l == r){
+        t[v] = a[l];//leaf
+    }
+    else{
+        int m = (l + r)/2;
+        build(2 * v, l, m);
+        build(2 * v + 1,m + 1, r);
+        t[v] = t[2 * v] + t[2 * v + 1];
+    }
+}
+ 
+int sum(int v, int tl, int tr, int l, int r){
+    if(l == tl && r == tr){
+        return t[v];
+    }
+    else{
+        int tm = (tl + tr)/2;
+        return sum(2 * v, tl, tm, l, min(tm, r)) + sum(2 * v + 1, tm + 1, tr, max(tm + 1, l), r);
+    }
+}
+ 
+void update(int v, int l, int r, int pos, int val){
+    if(l == r){
+        t[pos] = val;
+    }
+    else{
+        int m = (l + r)/2;
+        if(pos <= m){
+            update(2 * v + 1, m + 1, r, pos, val);
+        }
+        else{
+            update(2 * v + 1, m + 1, r, pos, val);
+        }
+        t[v] = t[2 * v] + t[2 * v + 1];
+    }
+}
